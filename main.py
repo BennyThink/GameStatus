5131ba3aa54f72f25e9473374f75a0a1270cb0be
Benny 小土豆 <benny.think@gmail.com>
2018-10-22 18:11:57 +0800

    disable scheduler while under dev environ

--- main.py
+++ main.py
@@ -244,9 +244,11 @@ if __name__ == "__main__":
     p = options.options.p
     h = options.options.h
 
-    # scheduler = BackgroundScheduler(timezone="Asia/Shanghai")
-    # scheduler.add_job(game_sync, 'interval', minutes=10)
-    # scheduler.add_job(web_sync, 'interval', minutes=10)
-    # scheduler.add_job(ss_sync, 'interval', minutes=60)
-    # scheduler.start()
+    scheduler = BackgroundScheduler(timezone="Asia/Shanghai")
+    scheduler.add_job(game_sync, 'interval', minutes=10)
+    scheduler.add_job(web_sync, 'interval', minutes=10)
+    scheduler.add_job(ss_sync, 'interval', minutes=60)
+    if not os.environ.get('dev'):
+        logging.info('Starting scheduler...')
+        scheduler.start()
     RunServer.run_server(port=p, host=h)
